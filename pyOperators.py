ac1d85698de4e5b5490d3dab7ae970aa64423693
Karaokesnr <ann164628@gmail.com>
2022-02-02 16:21:18 +0300

    Python Operators

---
+++ pyOperators.py
@@ -0,0 +1,17 @@
+#python operators
+a = 21//5
+b = 21/5
+print(type(a))
+print(type(b))
+
+#arithmetic operators
+print("Arithmetic operators")
+a = 10
+b = 5
+print ("Addition: ",a+b)
+print ("Subtraction: ",a-b)
+print ("Multiplacaton: ",a*b)
+print ("Division :",a/b)
+print ("Floor Division: ",a//b)
+print ("Modulus: ", a%b)
+print ("Exponent: ", a**b)
