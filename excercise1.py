afcb4f9e305b6e77be0cf2b53f24272d6bb089a7
Karaokesnr <ann164628@gmail.com>
2022-02-02 16:16:25 +0300

    Excercise 1

---
+++ excercise1.py
@@ -0,0 +1,15 @@
+#excercise
+#write a programm to check whether a number is divisible by 2. 
+#prompt the user to enter the number
+
+no = int(input("Enter the number to check if it is divisible by 2: "))
+if no%2 == 0:
+    print("The number is divisible by 2")
+else:
+    print ("The number is not divisible by 2")
+    
+nu = int(input("Enter the number to check if it is divisible by 5: "))
+if no%5 == 0:
+    print("The number is divisible by 5")
+else:
+    print ("The number is not divisible by 5")
